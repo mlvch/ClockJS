e44e57a06d34ceec546bcf0e9d9eeafec63e3c09
Dmitriy <itsmalevich@gmail.com>
2020-10-16 12:25:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]'),
+  minuteHand = document.querySelector('[data-minute-hand]'),
+  secondHand = document.querySelector('[data-second-hand]');
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+
+  console.log(minuteHand);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360);
+} 
